ebb9cb76ede23e6b0df0b121fa363a5a31f7d209
Calorindo <114764344+Calorindo@users.noreply.github.com>
2026-01-27 09:57:29 -0300

    feat: change to hash router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
 import Admin from './pages/Admin';
 import Login from './pages/Login';
